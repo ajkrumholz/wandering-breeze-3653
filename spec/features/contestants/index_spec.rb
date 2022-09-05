cceea52b5adf3394f7900c5fe7470a3422e7bbde
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-05 17:48:15 -0400

    feat: add contestant names and projects to project index page

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe 'contestant index' do
+  before :each do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+    visit "/contestants"
+  end
+
+    # As a visitor,
+    # When I visit the contestants index page ("/contestants")
+    # I see a list of names of all the contestants
+    # And under each contestants name I see a list of the projects (names) that they've been on
+    
+    # (e.g.   Kentaro Kameyama
+    #         Projects: Litfit, Rug Tuxedo
+    
+    #         Jay McCarroll
+    #         Projects: LeatherFeather)
+
+  describe 'when I visit the contestant index page' do
+    it 'displays the names of all the contestants' do
+      Contestant.all.each do |contestant|
+        expect(page).to have_content(contestant.name)
+      end
+    end
+
+    it 'displays a list of project names each contestant has been on' do
+      Contestant.all.each do |contestant|
+        within("#contestant-#{contestant.id}") do
+          contestant.projects.each do |project|
+            expect(page).to have_content(project.name)
+          end
+        end
+      end
+    end
+  end
+end
